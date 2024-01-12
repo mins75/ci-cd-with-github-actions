424f8f54683a303c56917c929e4ad84880892a33
mins75 <94439376+mins75@users.noreply.github.com>
2024-01-12 23:53:57 +0100

    Update test_e2e_app.py

--- test_e2e_app.py
+++ test_e2e_app.py
@@ -2,6 +2,8 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By #Added
+import chromedriver_autoinstaller
+
 #import time
 
 class TestAppE2E(unittest.TestCase):
@@ -9,7 +11,7 @@ class TestAppE2E(unittest.TestCase):
         # Launch your flask app first
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(executable_path=chromedriver_autoinstaller.install())
         self.driver.get('http://localhost:5000') 
 
     def test_add_and_delete_item(self):
